19bcb733e0b921fc87089b5878c34bd77af3f399
Bennett Levine <ben.lvn@gmail.com>
2018-12-17 23:47:57 -0500

    Built extension with working functionality and icons

---
+++ background.js
@@ -0,0 +1,17 @@
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.browserAction.onClicked.addListener(function() {
+    console.log("Icon clicked")
+    gotoVideo()
+  })
+});
+
+function gotoVideo() {
+  fetch('video_links.txt')
+    .then((response) => response.text())
+    .then((text) => {
+      ids = text.split("\n")
+      baseurl = "https://www.youtube.com/watch?v="
+      random_id = ids[Math.floor(Math.random()*ids.length)]
+      chrome.tabs.create({url: baseurl + random_id})
+    })
+};
